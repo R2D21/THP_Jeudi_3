6cd779ca28783d3302e5fc2b0f9d45858ff889a1
eans25 <eangly.eng@gmail.com>
2018-07-19 23:09:07 +0200

    send mail ok

--- app.rb
+++ app.rb
@@ -7,8 +7,44 @@ $LOAD_PATH.unshift File.expand_path('./../lib', __dir__)
 require_relative './lib/app/townhalls_mailer'
 require_relative './lib/app/townhalls_scrapper'
 
-
+puts 'BONJOUR! Tout d\'abord, , nous allons scrapper tous les mails des mairies du département 10...'
+puts '(Cela peut prendre du temps suivant votre connexion, désolé...)'
 
 
 save_cities
-# Mailer.new.perform
+
+
+puts 'Bien! le fichier a été sauvegardé dans un fichier JSON dans le dossier /db'
+
+puts 'Souhaitez vous envoyer le message de THP aux mairies (tapez 1)'
+
+# @json_file_created = 0
+
+loop do
+  puts '(press 0 to quit)'
+  print '> '
+
+
+   choice = gets.chomp.to_i
+
+
+   unless [0, 1].include?(choice)
+    loop do
+      puts 'Wrong entry. Please choose 1 or 2:'
+      print '> '
+      choice = gets.chomp.to_i
+
+      break if [1].include?(choice)
+    end
+  end
+
+    if choice.zero?
+    puts 'Au revoir!'
+    break
+  end
+
+
+  if choice == 1
+Mailer.new.perform
+end
+end
--- lib/app/townhalls_mailer.rb
+++ lib/app/townhalls_mailer.rb
@@ -3,18 +3,18 @@ require 'json'
 
 class Mailer
   attr_accessor :username, :password, :gmail
+  
   def initialize
     puts "Connexion à ton compte MAIL pour envoyer les mails aux mairies"
-   puts "Entre ton mail (GMAIL)"
- print ">"
+    puts "Entre ton mail (GMAIL)"
+    print ">"
     @username = gets.chomp
-  puts "Entre ton mot de passe"
-  print ">"
+    puts "Entre ton mot de passe"
+    print ">"
     @password = gets.chomp
-    puts "Vos mails ont été envoyé!!!!!"
 
     @gmail = Gmail.connect(username, password)
-    end
+  end
 
   def send_mail(commune, my_mail)
     @gmail.deliver do
@@ -26,18 +26,19 @@ class Mailer
       html_part do
         content_type 'text/html; charset=UTF-8'
         body "Bonjour,\n
-Je m'appelle Henry LENG, je suis élève à The Hacking Project, une formation au code gratuite, sans locaux, sans sélection, sans restriction géographique.\n
-La pédagogie de ntore école est celle du peer-learning, où nous travaillons par petits groupes sur des projets concrets qui font apprendre le code.\n
-Le projet du jour est d'envoyer (avec du codage) des emails aux mairies pour qu'ils nous aident à faire de The Hacking Project un nouveau format d'éducation pour tous.\n
+          Je m'appelle Henry LENG, je suis élève à The Hacking Project, une formation au code gratuite, sans locaux, sans sélection, sans restriction géographique.\n
+          La pédagogie de ntore école est celle du peer-learning, où nous travaillons par petits groupes sur des projets concrets qui font apprendre le code.\n
+          Le projet du jour est d'envoyer (avec du codage) des emails aux mairies pour qu'ils nous aident à faire de The Hacking Project un nouveau format d'éducation pour tous.\n \n
 
-Déjà 500 personnes sont passées par The Hacking Project. Est-ce que votre mairie veut changer le monde avec nous ?\n
-Charles, co-fondateur de The Hacking Project pourra répondre à toutes vos questions : 06.95.46.60.80"
+          Déjà 500 personnes sont passées par The Hacking Project. Est-ce que votre mairie veut changer le monde avec nous ?\n
+          Charles, co-fondateur de The Hacking Project pourra répondre à toutes vos questions : 06.95.46.60.80"
       end
     end
+    puts "Vos mails ont été envoyé!!!!!"
   end
 
   def perform
-    json = File.read('./db/hola.json')
+    json = File.read('./db/town_halls.json')
     hash = JSON.parse(json)
     hash.each do |k, v|
       send_mail(k, v)
