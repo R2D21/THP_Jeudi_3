478bc579219cd59ed8a14f9ffaebae635ae7d64b
kwevan gouacide <kyg972@msn.com>
2018-07-19 12:36:35 +0200

    modif mailer

--- lib/app/townhalls_mailer.rb
+++ lib/app/townhalls_mailer.rb
@@ -1,38 +1,36 @@
 require 'gmail'
 require 'json'
 
+class Mailer
+  attr_accessor :username, :password, :gmail
+  def initialize
+    @username = 'thp.thehackingproject@gmail.com'
+    @password = 'av'
+    @gmail = Gmail.connect(username, password)
+    end
 
-def send_mail(my_mail)
-
-  username = "thp.thehackingproject@gmail.com"
-  password = ENV["GMAIL_PASSWORD"]
-
-  p gmail = Gmail.connect(username, password)
-
-gmail.deliver do
-  to my_mail
-  subject "Having fun in Puerto Rico!" #objet
-  text_part do
-    body "Text of plaintext message." #aucune idée de ce que ça fait
+  def send_mail(my_mail)
+    @gmail.deliver do
+      to my_mail
+      subject 'Having fun in Puerto Rico!' # objet
+      text_part do
+        body 'Text of plaintext message.' # aucune idée de ce que ça fait
+      end
+      html_part do
+        content_type 'text/html; charset=UTF-8'
+        body '<p>Text of <em>html</em> message.</p>' # message
+      end
+    end
   end
-  html_part do
-    content_type 'text/html; charset=UTF-8'
-    body "<p>Text of <em>html</em> message.</p>" #message
-  end
-  # add_file "/path/to/some_image.jpg"
-end
 
-end
-
-# send_mail("kyg972@msn.com")
-
-
-
-json = File.read('hola.json')
+  def perform
+    json = File.read('../../db/hola.json')
     hash = JSON.parse(json)
-p hash
-
-hash.each do |k, v|
-send_mail(v)
-
+    hash.each do |_k, v|
+      send_mail(v)
+    end
+    @gmail.logout
+  end
 end
+
+Mailer.new.perform
