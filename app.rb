77777c829ac1391205595631a607d43853db3de2
eans25 <eangly.eng@gmail.com>
2018-07-19 22:18:26 +0200

    app modif

--- app.rb
+++ app.rb
@@ -8,7 +8,15 @@ require_relative './lib/app/townhalls_mailer'
 require_relative './lib/app/townhalls_scrapper'
 
 
-
+puts 'Welcome! First of all, let me scrap the emails of all town halls of the 10th department...'
+puts '(it may take few minutes depending on your connection, sorry)'
 
 save_cities
+
+puts ' =======> the files has been saved on ./lib/town_halls.json'
+
+
+
+
+
 # Mailer.new.perform
