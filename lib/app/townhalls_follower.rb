1d3f5daa964275de434eac08d294c09fe5dcc206
Amélie Foulquier <amelie.foulquier@gmail.com>
2018-07-20 02:00:54 +0200

    modif townhalls_follower

--- lib/app/townhalls_follower.rb
+++ lib/app/townhalls_follower.rb
@@ -1,4 +1,3 @@
-# ligne très importante qui appelle la gem. Sans elle, le programme ne saura pas appeler Twitter
 require 'twitter'
 require 'dotenv'
 require 'pry'
@@ -12,40 +11,57 @@ Dotenv.load
     config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
     config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
   end
-@client = client
-# fichier = File.read('.fichier.json') #lecture du fichier to_json
-# obj_hash = JSON.parse(fichier) #transportation(parsing) des données en JavaScript Obj)
-# users_twt = []
+  @client = client
+
+def extract_data
+
+  fichier = File.read('./db/town_halls.json') #lecture du fichier to_json
+  new_hash = JSON.parse(fichier) #transportation(parsing) des données
+
+  # création d'un tableau avec le nom des villes
+  names = []
+  new_hash.keys.each do |k|
+    names << k
+  end
+  @names = names
+
+end
 
 def search_accounts
 
-  names = ["bordeaux", "marseille", "aubervilliers"]
+
   users = []
-  names.each do |name|
-    if @client.user_search(name)[0] == nil
-      users << " "
-    else
-      users << @client.user_search(name)[0]
+  @names.each do |name|
+
+    begin
+      if @client.user_search(name)[0] == nil
+        users << " "
+      else
+        users << @client.user_search(name)[0]
+      end
+    rescue Twitter::Error::TooManyRequests, Twitter::Error::BadRequest, Twitter::Error::NotFound
+      next
+    end
   end
 
-end
   @users = users
-  # #envoie les comptes Twitter dans le fichier json
-  # File.open("./db/...json", "w") do |f|
-  #   f.write(obj.to_json)
-  # end
 
 end
-
 
 def follow_accounts(users) #follow les differents comptes Twitter
 
-  @users.each do |user|
-    @client.follow(user)
+  begin
+    @users.each do |user|
+      @client.follow(user)
+
+    rescue Twitter::Error::TooManyRequests, Twitter::Error::BadRequest, Twitter::Error::NotFound
+      next
+    end
   end
 end
 
 def perform
+  extract_data
   search_accounts
   follow_accounts(search_accounts)
 end
